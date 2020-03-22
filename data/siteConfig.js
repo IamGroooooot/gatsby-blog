b77dbcd77d0a757212671c989df170fdd0d18b3e
Juhyeong Ko <dury.ko@gmail.com>
2020-03-22 14:13:40 +0900

    Update siteConfig.js

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -1,14 +1,14 @@
 module.exports = {
-  siteTitle: 'ÇïÃ¢ °³¹ßÀÚ',
-  siteDescription: 'A Gatsby theme/starter to build lightning-fast websites',
-  authorName: 'ÁÖÇü',
+  siteTitle: 'í—¬ì°½ ê°œë°œì',
+  siteDescription: 'Coding Blog',
+  authorName: 'ì£¼í˜•',
   twitterUsername: '_blank',
   authorAvatar: 'avatar.jpeg', // file in content/images
   defaultLang: 'en', // show flag if lang is not default. Leave empty to enable flags in post lists
   authorDescription: `
   For the last decade, Maxence Poutord has worked with a variety of web technologies. He is currently focused on front-end development.
   On his day to day job, he is working as a senior front-end engineer at VSware. He is also an occasional tech speaker and a mentor.
-  As a digital nomad, he is living where the WiFi and sun are ğŸ˜ <br>
+  As a digital nomad, he is living where the WiFi and sun are Ã°Å¸ËœÅ½ <br>
   Do you want to know more? <a href="https://www.maxpou.fr/about" target="_blank">Visit my website!</a>
   `,
   siteUrl: 'https://helldev.netlify.com/',
